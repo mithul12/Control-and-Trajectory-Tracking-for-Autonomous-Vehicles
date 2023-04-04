0cf19c405477edfed9869b5c8d0d84211a21a299
Mithul Avinash <118648632+mithul12@users.noreply.github.com>
2023-04-04 20:22:07 +0530

    Add files via upload

---
+++ project/pid controller/Cmakefiles/pid_controller.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/haoquan/Documents/Dev/nd013-c6-control-starter/project/pid_controller/behavior_planner_FSM.cpp" "CMakeFiles/pid_controller.dir/behavior_planner_FSM.cpp.o" "gcc" "CMakeFiles/pid_controller.dir/behavior_planner_FSM.cpp.o.d"
+  "/Users/haoquan/Documents/Dev/nd013-c6-control-starter/project/pid_controller/cost_functions.cpp" "CMakeFiles/pid_controller.dir/cost_functions.cpp.o" "gcc" "CMakeFiles/pid_controller.dir/cost_functions.cpp.o.d"
+  "/Users/haoquan/Documents/Dev/nd013-c6-control-starter/project/pid_controller/cubic_spiral.cpp" "CMakeFiles/pid_controller.dir/cubic_spiral.cpp.o" "gcc" "CMakeFiles/pid_controller.dir/cubic_spiral.cpp.o.d"
+  "/Users/haoquan/Documents/Dev/nd013-c6-control-starter/project/pid_controller/integral.cpp" "CMakeFiles/pid_controller.dir/integral.cpp.o" "gcc" "CMakeFiles/pid_controller.dir/integral.cpp.o.d"
+  "/Users/haoquan/Documents/Dev/nd013-c6-control-starter/project/pid_controller/main.cpp" "CMakeFiles/pid_controller.dir/main.cpp.o" "gcc" "CMakeFiles/pid_controller.dir/main.cpp.o.d"
+  "/Users/haoquan/Documents/Dev/nd013-c6-control-starter/project/pid_controller/motion_planner.cpp" "CMakeFiles/pid_controller.dir/motion_planner.cpp.o" "gcc" "CMakeFiles/pid_controller.dir/motion_planner.cpp.o.d"
+  "/Users/haoquan/Documents/Dev/nd013-c6-control-starter/project/pid_controller/pid_controller.cpp" "CMakeFiles/pid_controller.dir/pid_controller.cpp.o" "gcc" "CMakeFiles/pid_controller.dir/pid_controller.cpp.o.d"
+  "/Users/haoquan/Documents/Dev/nd013-c6-control-starter/project/pid_controller/spiral_base.cpp" "CMakeFiles/pid_controller.dir/spiral_base.cpp.o" "gcc" "CMakeFiles/pid_controller.dir/spiral_base.cpp.o.d"
+  "/Users/haoquan/Documents/Dev/nd013-c6-control-starter/project/pid_controller/spiral_equations.cpp" "CMakeFiles/pid_controller.dir/spiral_equations.cpp.o" "gcc" "CMakeFiles/pid_controller.dir/spiral_equations.cpp.o.d"
+  "/Users/haoquan/Documents/Dev/nd013-c6-control-starter/project/pid_controller/utils.cpp" "CMakeFiles/pid_controller.dir/utils.cpp.o" "gcc" "CMakeFiles/pid_controller.dir/utils.cpp.o.d"
+  "/Users/haoquan/Documents/Dev/nd013-c6-control-starter/project/pid_controller/velocity_profile_generator.cpp" "CMakeFiles/pid_controller.dir/velocity_profile_generator.cpp.o" "gcc" "CMakeFiles/pid_controller.dir/velocity_profile_generator.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project/pid controller/Cmakefiles/pid_controller.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pid_controller.dir/behavior_planner_FSM.cpp.o"
+  "CMakeFiles/pid_controller.dir/behavior_planner_FSM.cpp.o.d"
+  "CMakeFiles/pid_controller.dir/cost_functions.cpp.o"
+  "CMakeFiles/pid_controller.dir/cost_functions.cpp.o.d"
+  "CMakeFiles/pid_controller.dir/cubic_spiral.cpp.o"
+  "CMakeFiles/pid_controller.dir/cubic_spiral.cpp.o.d"
+  "CMakeFiles/pid_controller.dir/integral.cpp.o"
+  "CMakeFiles/pid_controller.dir/integral.cpp.o.d"
+  "CMakeFiles/pid_controller.dir/main.cpp.o"
+  "CMakeFiles/pid_controller.dir/main.cpp.o.d"
+  "CMakeFiles/pid_controller.dir/motion_planner.cpp.o"
+  "CMakeFiles/pid_controller.dir/motion_planner.cpp.o.d"
+  "CMakeFiles/pid_controller.dir/pid_controller.cpp.o"
+  "CMakeFiles/pid_controller.dir/pid_controller.cpp.o.d"
+  "CMakeFiles/pid_controller.dir/spiral_base.cpp.o"
+  "CMakeFiles/pid_controller.dir/spiral_base.cpp.o.d"
+  "CMakeFiles/pid_controller.dir/spiral_equations.cpp.o"
+  "CMakeFiles/pid_controller.dir/spiral_equations.cpp.o.d"
+  "CMakeFiles/pid_controller.dir/utils.cpp.o"
+  "CMakeFiles/pid_controller.dir/utils.cpp.o.d"
+  "CMakeFiles/pid_controller.dir/velocity_profile_generator.cpp.o"
+  "CMakeFiles/pid_controller.dir/velocity_profile_generator.cpp.o.d"
+  "pid_controller"
+  "pid_controller.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pid_controller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
